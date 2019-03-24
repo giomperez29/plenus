290b0af0dd4df53a2e7de6a334de8dc762169a65
giomperez29 <giomperez@hotmail.es>
2019-03-24 13:10:00 -0700

    js up

---
+++ js/propio.js
@@ -0,0 +1,12 @@
+$(document).ready(function () {
+
+    $('h1').click(function () {
+      // alert("Me has dado click");
+        $("#destinos").css('background-color','blue');
+    });
+
+    $('.promo').text("Orlando");
+
+
+
+});
